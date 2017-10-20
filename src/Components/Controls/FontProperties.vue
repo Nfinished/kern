5a03b49f39a8354e522760652b5de32a637cccb5
Nfinished <we.have@unfinished.business>
2017-10-20 14:55:31 -0400

    font variant to variant

--- src/Components/Controls/FontProperties.vue
+++ src/Components/Controls/FontProperties.vue
@@ -16,7 +16,7 @@
         </select>
       </div> -->
       <div class="form-group">
-        <label>Font Variant <span class="rule-name">(font-variant)</span></label>
+        <label>Variant <span class="rule-name">(font-variant)</span></label>
         <ToggleButton @change="ruleStore['font-variant'] = ruleStore['font-variant'] === 'normal' ? 'small-caps' : 'normal'" color="#9b4dca" />
         <label class="toggle-label" :class="{disabled: ruleStore['font-variant'] === 'normal'}">small-caps</label>
       </div>
